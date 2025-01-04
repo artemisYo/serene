3e1281e6bf64a5547ff929b2e4039ff04267a33e
ArtemisYo <art.stellata@proton.me>
2025-01-04 20:50:07 +0100

    forgor free macro

--- src/lib.h
+++ src/lib.h
@@ -7,6 +7,8 @@
 #define serene_calloc(a, n, T) a.alloc(a.ctx, n * sizeof(T))
 #define serene_balloc(a, b) a.alloc(a.ctx, b)
 
+#define serene_free(a, p) a.free(a.ctx, p);
+
 struct Allocator {
     void* ctx;
     void* (*alloc)(void*, size_t);
